6c7d89116b0c1381c229bf27b6dd7ed6f6e331b0
Tani314 <tanitabassum299@gmail.com>
2019-10-19 15:56:34 -0400

    more

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 
 import React from 'react'
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-import Home from './Home'
 import AllOwners from './AllOwners'
 import AllSitters from './AllSitters'
 import SingleOwner from './SingleOwner';
 import SingleSitter from './SingleSitter';
-import UpdateOwner from './UpdateOwner'
-import UpdateSitter from './UpdateSitter'
-import Navbar from './Navbar'
-import Error from './Error'
-
+import SecondPage from './SecondPage'
 const App = () => {
  return (
    <Router>
       <div>
-     <Navbar/>
      <Switch>
-        <Route exact path ='/' component ={Home} />
+        <Route exact path= '/secondPage' component={SecondPage}/>
         <Route exact path ='/owners' component={AllOwners}/>
         <Route  exact path ='/sitters' component={AllSitters}/>
         <Route exact path= '/owners/:ownerId' component={SingleOwner}/>
         <Route exact path= '/sitters/:sitterId' component={SingleSitter}/>
-        <Route exact path= '/owners/:ownerId' component={UpdateOwner}/>
-        <Route exact path= '/sitters/:sitterId' component={UpdateSitter}/>
-        <Route component={Error}/>
      </Switch>
    </div>
    </Router>
@@ -36,4 +23,4 @@ const App = () => {
  )
 }
 
-export default App;
+export default App
